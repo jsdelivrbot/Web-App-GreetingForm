36480e9bbee65447223e9608ffe30234a6fe93ee
Thabiso22 <thabisobudaza@gmail.com>
2017-08-28 15:41:39 +0200

    modified

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var exphbs =require ("express-handlebars");
-
 var app = express();
 //body Parser middleware
 app.use(bodyParser.urlencoded({extended:false}));
@@ -21,11 +20,7 @@ app.set("view engine","handlebars");
 app.get("/", function (req,res) {
   res.render("greetings");
 
-
 });
-//radio buttons reaction
-
-
 
 // //display textbox input
 app.post("/greeted" ,function (req,res){
@@ -33,11 +28,36 @@ app.post("/greeted" ,function (req,res){
   var textVal =req.body.textbox;
   var greetedUser = "";
 
+
+//////Test counter
+var greeted=[];
+var textVal = req.params.textbox;
+
+greeted.push(textVal);
+
+
+var textVal = req.params.textbox;
+greeted.forEach(function(textVal) {
+if(greeted[textVal] === undefined) {
+  greeted[textVal] = 0;
+}
+  greeted[textVal] = greeted[textVal] + 1;
+});
+console.log(greeted);
+
+
+res.send(req.params.textbox  + " has been greeted " + greeted[textVal] + " time(s)");
+
+
+
+
+//////////////////Test counter
+
 if (language === "isiXhosa") {
    greetedUser = "Molo " + textVal;
 }
 else if (language === "English") {
-  greetedUser = "Hallo " + textVal;
+  greetedUser = "Hello " + textVal;
 }
 else if (language === "Afrikaans") {
   greetedUser = "Halo " + textVal;
@@ -47,16 +67,9 @@ else if (language === "Afrikaans") {
 
   });
 
-
 });
 
  var server = app.listen(3000, function () {
  var host = server.address().address;
 var port = server.address().port;
  });
-
-// var host = process.env.HOST || "http://localhost";
-// var port = process.env.PORT || 3000;
-//
-//
-//  console.log('Example app listening at %s:%s', host, port);
